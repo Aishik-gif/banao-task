8628b1e36dfe45fe23d167059bc1fde8700ac417
Aishik Dutta <aishikd2@gmail.com>
2024-07-12 03:31:53 +0530

    init

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.transporter = void 0;
 const userRouter_1 = __importDefault(require("./routes/userRouter"));
@@ -11,6 +10,7 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use('/api/v1/user', userRouter_1.default);
@@ -23,4 +23,4 @@ exports.transporter = nodemailer_1.default.createTransport({
         pass: process.env.APP_PASSWORD,
     },
 });
-app.listen((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000);
+app.listen(PORT);
